3d7ea616b22fbb66ea8230979e40db5ae30adaf3
Evan Smith <evan.leigh@outlook.com>
2024-05-13 20:46:03 -0400

    feat: added back in `showKey` to configuration

--- components/settings/window.tsx
+++ components/settings/window.tsx
@@ -2,111 +2,116 @@
 
 import { useEffect, useRef, useState } from "react";
 
-import { IoIosCloseCircleOutline } from "react-icons/io";
-
 import { useSettings } from "@/components/settings/context";
+
 import { ConfigType } from "@/types";
+
 import { useTheme } from "next-themes";
+
+import { Card } from "../ui/card";
+import { Checkbox } from "../ui/checkbox";
 
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
-import { Card } from "../ui/card";
+} from "@/components/ui/select";
 
-const layout_options = [
-  "Grid View", "List View", "Detail View",
-  "Card View", "Table View", "Compact View",
-  "Tile View",
-];
+import { FiX } from "react-icons/fi";
 
-const SettingsWindow: React.FC<{ onClose: () => void }> = ({ onClose }) => {
+interface SettingsWindowProps {
+  onClose: () => void;
+}
+
+const layoutOptions = ["Grid View", "List View", "Detail View", "Card View", "Table View", "Compact View", "Tile View"]
+
+const SettingsWindow: React.FC<SettingsWindowProps> = ({ onClose }) => {
   const { config, setConfig } = useSettings();
+  const { setTheme } = useTheme();
   const [updatedConfig, setUpdatedConfig] = useState<ConfigType>(config);
+  const [isSaved, setIsSaved] = useState(false);
+  const [dragging, setDragging] = useState(false);
+  const [offset, setOffset] = useState({ offsetX: 0, offsetY: 0 });
   const settingsWindowRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (settingsWindowRef.current !== null) {
-      const rect = settingsWindowRef.current.getBoundingClientRect();
-      const { innerWidth, innerHeight } = window;
-      const initialLeft = (innerWidth - rect.width) / 2;
-      const initialTop = (innerHeight - rect.height) / 2;
+  const Dropdown = ({ name, options, value, onChange }: any) => {
+    return (
+      <Select
+        value={value}
+        onValueChange={(newValue: string) => {
+          onChange({
+            target: {
+              name,
+              value: newValue,
+              type: "select"
+            }
+          });
+        }}
+      >
+        <SelectTrigger className="w-48" value={value}>
+          <SelectValue placeholder="Select an option" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {options.map((option: string) => (
+              <SelectItem key={option} value={option}>
+                {option}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    );
+  };
 
-      settingsWindowRef.current.style.left = `${initialLeft}px`;
-      settingsWindowRef.current.style.top = `${initialTop}px`;
-    }
+  // Position the settings window in the center
+  useEffect(() => {
+    centerWindow();
   }, []);
 
   useEffect(() => {
     setUpdatedConfig(config);
   }, [config]);
 
-  const isCheckboxInput = (
-    element: HTMLInputElement | HTMLSelectElement,
-    type: string
-  ): element is HTMLInputElement => {
-    return element instanceof HTMLInputElement && type === "checkbox";
+  // Handle window dragging
+  const centerWindow = () => {
+    if (settingsWindowRef.current) {
+      const rect = settingsWindowRef.current.getBoundingClientRect();
+      const { innerWidth, innerHeight } = window;
+      settingsWindowRef.current.style.left = `${(innerWidth - rect.width) / 2}px`;
+      settingsWindowRef.current.style.top = `${(innerHeight - rect.height) / 2}px`;
+    }
   };
 
-  const updateStateKeys = (prevState: ConfigType, name: string, value: any): ConfigType => {
-    if (!prevState.keys) return prevState;
+  const handleMouseMove = (event: MouseEvent) => { moveWindow(event.clientX, event.clientY); };
+  const handleMouseDown = (event: React.MouseEvent) => { startDragging(event.clientX, event.clientY); };
 
-    const newKeys = Object.assign({}, prevState.keys);
-    newKeys[name] = value;
-
-    return Object.assign({}, prevState, {
-      keys: newKeys,
-    });
+  const startDragging = (clientX: number, clientY: number) => {
+    if (settingsWindowRef.current) {
+      const rect = settingsWindowRef.current.getBoundingClientRect();
+      setOffset({
+        offsetX: clientX - rect.left,
+        offsetY: clientY - rect.top,
+      });
+      setDragging(true);
+      document.body.style.userSelect = "none";
+    }
   };
 
-  const updateState = (prevState: ConfigType, name: string, value: any): ConfigType => {
-    return Object.assign({}, prevState, {
-      [name]: value,
-    });
+  const moveWindow = (clientX: number, clientY: number) => {
+    if (settingsWindowRef.current) {
+      const { width, height } = settingsWindowRef.current.getBoundingClientRect();
+
+      const newX = Math.min(Math.max(clientX - offset.offsetX, 0), window.innerWidth - width);
+      const newY = Math.min(Math.max(clientY - offset.offsetY, 0), window.innerHeight - height);
+
+      settingsWindowRef.current.style.left = `${newX}px`;
+      settingsWindowRef.current.style.top = `${newY}px`;
+    }
   };
-
-  const [isSaved, setIsSaved] = useState(false);
-  const [isReset, setIsReset] = useState(false);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const { name, value, type } = event.target;
-    const updatedValue = isCheckboxInput(event.target, type) ? event.target.checked : value;
-
-    const newConfig =
-      updatedConfig.keys && name in updatedConfig.keys
-        ? updateStateKeys(updatedConfig, name, updatedValue)
-        : updateState(updatedConfig, name, updatedValue);
-
-    setUpdatedConfig(newConfig);
-    // Removed setConfig(newConfig); to avoid immediate update of the global config
-  };
-
-  const saveConfig = () => {
-    setConfig(updatedConfig); // Update the global config only when saving
-    setIsSaved(true);
-
-    setTimeout(() => {
-      setIsSaved(false);
-    }, 2000);
-  };
-
-  const resetConfig = () => {
-    setUpdatedConfig(config);
-    setIsReset(true);
-
-    setTimeout(() => {
-      setIsReset(false);
-    }, 2000);
-  }
-
-  const { setTheme } = useTheme();
-
-  const [dragging, setDragging] = useState(false);
 
   useEffect(() => {
     if (dragging) {
@@ -115,188 +120,114 @@ const SettingsWindow: React.FC<{ onClose: () => void }> = ({ onClose }) => {
     }
 
     return () => {
-      if (dragging) {
-        document.removeEventListener("mousemove", handleMouseMove);
-      }
+      if (dragging) { document.removeEventListener("mousemove", handleMouseMove); }
     };
+
   }, [dragging]);
 
-  useEffect(() => {
-    if (!dragging) {
-      document.body.style.userSelect = "";
-    }
-  }, [dragging]);
-
-  const [offset, setOffset] = useState({ offsetX: 0, offsetY: 0 });
-
-  const handleMouseDown = (event: React.MouseEvent) => {
-    if (settingsWindowRef.current !== null) {
-      let offsetX = event.clientX - settingsWindowRef.current.getBoundingClientRect().left;
-      let offsetY = event.clientY - settingsWindowRef.current.getBoundingClientRect().top;
-
-      setOffset({ offsetX, offsetY }); // Store offsets in state
-
-      setDragging(true);
-      document.body.style.userSelect = "none";
-    }
+  // Handle input change
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = event.target;
+    const newValue = value;
+    const newConfig = { ...updatedConfig, [name]: newValue };
+    setUpdatedConfig(newConfig);
   };
 
-  const handleMouseMove = (event: MouseEvent) => {
-    let clientX = event.clientX - offset.offsetX;
-    let clientY = event.clientY - offset.offsetY;
-
-    if (settingsWindowRef.current !== null) {
-      const { width, height } = settingsWindowRef.current.getBoundingClientRect();
-
-      // Boundary checks
-      const maxWidth = window.innerWidth;
-      const maxHeight = window.innerHeight;
-
-      if (clientX < 0) {
-        clientX = 0;
-      } else if (clientX + width > maxWidth) {
-        clientX = maxWidth - width;
-      }
-
-      if (clientY < 0) {
-        clientY = 0;
-      } else if (clientY + height > maxHeight) {
-        clientY = maxHeight - height;
-      }
-
-      settingsWindowRef.current.style.left = `${clientX}px`;
-      settingsWindowRef.current.style.top = `${clientY}px`;
-
-      setOffset({ offsetX: event.clientX - clientX, offsetY: event.clientY - clientY }); // Update offsets
+  // Handle checkbox change
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target;
+    // if the key isn't present in the table, instead add it to the upper level
+    if (!updatedConfig.keys[name]) {
+      const newConfig = { ...updatedConfig, [name]: checked }; setUpdatedConfig(newConfig);
+      return;
     }
+    const newConfig = { ...updatedConfig, keys: { ...updatedConfig.keys, [name]: checked } };
+    setUpdatedConfig(newConfig);
   };
 
-  // Sub-components
-  const Dropdown = ({ name, options, value, onChange }: any) => {
-    return (
-      <select
-        name={name}
-        value={value}
-        onChange={onChange}
-        className="h-10 rounded-md border px-4 pr-8 -outline-offset-1"
-      >
-        {options.map((option: string) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-    );
+  // Save config to local storage
+  const saveConfig = () => {
+    setConfig(updatedConfig);
+    setTheme(updatedConfig.theme);
+    setTimeout(() => setIsSaved(false), 1000);
+    setIsSaved(true);
   };
 
+  // Render settings UI
   return (
     <>
       {dragging && <div className="fixed inset-0 z-10" style={{ pointerEvents: "none" }}></div>}
-
       <Card
         ref={settingsWindowRef}
         style={{ position: "fixed", left: "0", top: "0" }}
-        className="w-[50vw] border"
-      >
+        className="z-10 w-[50vw] border">
         <div
-          className="sticky top-0 flex cursor-move items-center rounded-t-md border-b p-2"
-          onMouseDown={handleMouseDown}
-        >
+          className="sticky top-0 flex cursor-move items-center rounded-t-md border-b p-2 overflow-y-scroll"
+          onMouseDown={handleMouseDown}>
           <h4 className="m-0">Settings</h4>
           <button onClick={onClose} className="ml-auto">
-            <IoIosCloseCircleOutline size={30} className="text-red-500 dark:text-red-400" />
+            <FiX size={30} className="text-slate-300" />
           </button>
         </div>
 
         <div className="flex max-h-[calc(70vh-80px)] flex-col gap-y-4 overflow-auto px-6 py-4">
-          <h2>Appearance</h2>
-          <hr />
+
+          <h2 className="text-2xl">Appearance</h2>
+
           <div>
             <label className="mr-3">Thumbnail Key:</label>
-            {
-              <Dropdown
-                name="thumbnailKey"
-                options={Object.keys(updatedConfig.keys)}
-                value={updatedConfig.thumbnailKey}
-                onChange={handleChange}
-              />
-            }
+            <Dropdown
+              name="thumbnailKey"
+              options={Object.keys(updatedConfig.keys)}
+              value={updatedConfig.thumbnailKey}
+              onChange={handleChange}
+            />
           </div>
 
           <div className="flex items-center">
-            <div className="h-full bg-[#888888]"></div>
+            <label className="mr-3">Show key:</label>
+            <Checkbox
+              name="showKey"
+              value={updatedConfig.showKey}
+              onCheckedChange={(checked) => handleCheckboxChange({ target: { name: "showKey", checked } })}
+            />
           </div>
-
-          <hr className="border-t" />
 
           {Object.entries(updatedConfig.keys).map(([key, value]) => (
             <div key={key} className="flex items-center">
               <label htmlFor={key} className="mr-3">
                 {key}:
               </label>
-              <div>
-                <input
-                  type="checkbox"
-                  id={key}
-                  name={key}
-                  checked={value as boolean}
-                  onChange={handleChange}
-                />
-              </div>
+              <Checkbox id={key} name={key} checked={value as boolean} onCheckedChange={(checked) => handleCheckboxChange({ target: { name: key, checked } })} />
             </div>
           ))}
 
-          <h2>Layout:</h2>
-
-          <hr className="border-t" />
-
+          <h2 className="text-2xl">Layout:</h2>
           <div className="flex items-center">
             <p className="mr-4">View:</p>
-            <select
+            <Dropdown
               name="layout"
+              options={layoutOptions}
               value={updatedConfig.layout}
               onChange={handleChange}
-              className="h-10 max-w-[120px] rounded-md border px-4 pr-8 -outline-offset-1"
-            >
-              {layout_options.map((layout) => (
-                <option key={layout} value={layout}>
-                  {layout}
-                </option>
-              ))}
-            </select>
+            />
           </div>
-
           <div className="flex items-center">
             <p className="mr-4">Theme:</p>
-            <select
+            <Dropdown
               name="theme"
-              className="h-10 rounded-md border px-4 pr-8 -outline-offset-1"
-            >
-              <option onClick={() => setTheme("system")} value="system">
-                System
-              </option>
-              <option onClick={() => setTheme("light")} value="light">
-                Light
-              </option>
-              <option onClick={() => setTheme("dark")} value="dark">
-                Dark
-              </option>
-            </select>
+              options={["system", "light", "dark"]}
+              value={updatedConfig.theme}
+              onChange={(e: any) => {
+                const theme = e.target.value.toLowerCase();
+                setUpdatedConfig({ ...updatedConfig, theme });
+              }}
+            />
           </div>
-
         </div>
         <div className="sticky bottom-0 flex items-center justify-start gap-3 rounded-b-md border-t p-2">
-          <button
-            onClick={saveConfig}
-            className="h-10 w-fit rounded-md px-4 -outline-offset-1 dark:bg-blue-500"
-          >
+          <button onClick={saveConfig} className="h-10 w-fit rounded-md px-4 -outline-offset-1 dark:bg-blue-500">
             {isSaved ? "Settings saved!" : "Save"}
-          </button>
-          <button
-            onClick={resetConfig}
-            className="border border-neutral-700 h-10 w-fit rounded-md px-4 -outline-offset-1"
-          >
-            {isReset ? "Settings reset!" : "Reset"}
           </button>
         </div>
       </Card>
